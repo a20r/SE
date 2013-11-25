abfd591802f27d9e40962188edc3af4073be5d7c
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-11-25 16:42:00 +0000

    Fixed the template and css issue

--- server/pageserver.py
+++ server/pageserver.py
@@ -1,15 +1,20 @@
 
 from flask import request, redirect, url_for, abort, jsonify, render_template
-
+from flask import make_response, Response
 from app import app
 
 STATIC_DIR = "static/"
 
+MIME_DICT = {
+    "js": "text/javascript",
+    "css": "text/css"
+}
+
 @app.route("/<filename>", methods = ["GET"])
 def serveHtmlPage(filename):
-    render_template(filename)
+    return render_template(filename)
 
 @app.route("/<filetype>/<filename>", methods = ["GET"])
 def serveScript(filetype, filename):
     with open(STATIC_DIR + filetype + "/" + filename) as f:
-        return f.read()
+        return Response(f.read(), mimetype = MIME_DICT[filetype])
