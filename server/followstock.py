8838187a5c70b1d8a52da9c0dab4c14e925a802d
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-11-25 19:40:09 +0000

    You can now not follow stocks you are already following

--- server/followstock.py
+++ server/followstock.py
@@ -14,18 +14,21 @@ def followStock():
 
     if len(userDataList) > 0:
         stockName = request.form["stock_name"]
-
-        r.table(db.USER_TABLE).get_all(
-            request.cookies.get(db.AUTH_COOKIE),
-            index = db.USER_SECONDARY_KEY
-        )[db.STOCKS_FOLLOWING_KEY].append(
-            stockName
-        ).run(db.CONN)
-
-        return jsonify(
-            error = 0,
-            message = "No error"
-        )
+        userData = userDataList[0]
+        if not stockName in userData[db.STOCKS_FOLLOWING_KEY]:
+            userData[db.STOCKS_FOLLOWING_KEY].append(stockName)
+            r.table(db.USER_TABLE).get(userData["username"]).update(
+                userData
+            ).run(db.CONN)
+            return jsonify(
+                error = 0,
+                message = "No error"
+            )
+        else:
+            return jsonify(
+                error = 1,
+                message = "User is already following that stock"
+            )
     else:
         resp = make_response(
             jsonify(
