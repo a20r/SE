27987dad8d55e913c6d2f8d746705f7c33ad66d5
Alexander James Wallar <aw204@st-andrews.ac.uk>
2013-11-26 10:22:09 +0000

    fixed da bug

--- server/financecache.py
+++ server/financecache.py
@@ -177,6 +177,63 @@ def updateAllHistorical():
         finally:
             db.MUTEX.release()
 
+@app.route("/follow", methods = ["POST"])
+def followStock():
+    global FOLLOWING_IN_USE
+
+    FOLLOWING_IN_USE = True
+
+    db.MUTEX.acquire()
+
+    try:
+        userData = r.table(db.USER_TABLE).get_all(
+            request.cookies.get(db.AUTH_COOKIE),
+            index = db.USER_SECONDARY_KEY
+        )[0].run(db.CONN)
+    finally:
+        db.MUTEX.release()
+
+    FOLLOWING_IN_USE = False
+    if userData:
+        stockName = request.form["stock_name"]
+        if not stockName in db.STOCK_MAP.keys():
+            return jsonify(
+                error = 1,
+                message = "Cannot follow an imaginary stock"
+            )
+
+        if not stockName in userData[db.STOCKS_FOLLOWING_KEY]:
+            userData[db.STOCKS_FOLLOWING_KEY].append(stockName)
+            FOLLOWING_IN_USE = True
+            try:
+                db.MUTEX.acquire()
+                r.table(db.USER_TABLE).get(userData["username"]).update(
+                    userData
+                ).run(db.CONN)
+            finally:
+                db.MUTEX.release()
+
+            FOLLOWING_IN_USE = False
+            return jsonify(
+                error = 0,
+                message = "No error"
+            )
+        else:
+            return jsonify(
+                error = 1,
+                message = "User is already following that stock"
+            )
+    else:
+        resp = make_response(
+            jsonify(
+                error = 1,
+                message = "User with associated token does not exist"
+            )
+        )
+
+        resp.set_cookie(db.AUTH_COOKIE, "")
+        return resp
+
 @app.route("/get_following", methods = ["GET"])
 def getFollowing():
     global FOLLOWING_IN_USE
--- server/followstock.py
+++ server/followstock.py
@@ -5,46 +5,6 @@ import rethinkdb as r
 from app import app, db
 import json
 
-@app.route("/follow", methods = ["POST"])
-def followStock():
-    userData = r.table(db.USER_TABLE).get_all(
-        request.cookies.get(db.AUTH_COOKIE),
-        index = db.USER_SECONDARY_KEY
-    )[0].run(db.CONN)
-
-    if userData:
-        stockName = request.form["stock_name"]
-        if not stockName in db.STOCK_MAP.keys():
-            return jsonify(
-                error = 1,
-                message = "Cannot follow an imaginary stock"
-            )
-
-        if not stockName in userData[db.STOCKS_FOLLOWING_KEY]:
-            userData[db.STOCKS_FOLLOWING_KEY].append(stockName)
-            r.table(db.USER_TABLE).get(userData["username"]).update(
-                userData
-            ).run(db.CONN)
-            return jsonify(
-                error = 0,
-                message = "No error"
-            )
-        else:
-            return jsonify(
-                error = 1,
-                message = "User is already following that stock"
-            )
-    else:
-        resp = make_response(
-            jsonify(
-                error = 1,
-                message = "User with associated token does not exist"
-            )
-        )
-
-        resp.set_cookie(db.AUTH_COOKIE, "")
-        return resp
-
 @app.route("/unfollow", methods = ["POST"])
 def unfollow():
     pass
