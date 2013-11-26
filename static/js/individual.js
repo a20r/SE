6385c82a5325aadf8af0b1e99f7183b5bb50bc39
mhristova <mhristova6@gmail.com>
2013-11-26 10:47:14 +0000

    more bug fix

--- static/js/individual.js
+++ static/js/individual.js
@@ -48,3 +48,8 @@ function checkLogin() {
 	}
 }
 
+function logout() {
+	document.cookie = 'stock_auth_token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+	window.location.replace("/main_page.html");
+}
+
