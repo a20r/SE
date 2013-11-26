e26463de832603b1b08fc7f15dcbf192a0e6d7f0
mhristova <mhristova6@gmail.com>
2013-11-26 10:44:06 +0000

    bug

--- static/js/individual.js
+++ static/js/individual.js
@@ -33,3 +33,18 @@ function follow() {
     });
 }
 
+function checkLogin() {
+	var login = document.getElementById("logCond");
+	var cookie = document.cookie;
+	if (cookie.indexOf("stock_auth_token=") !== -1) {
+		var cookieValue = cookie.indexOf(cookie.indexOf("stock_auth_token="));
+		if (cookieValue !== -1) {
+			login.innerHTML = '<a href="javascript:logout();">LOGOUT</a>'
+		} else {
+			login.innerHTML = '<a href="/login.html">LOGIN</a>';
+		}
+	} else {
+		login.innerHTML = '<a href="/login.html">LOGIN</a>';
+	}
+}
+
