1d18bb5fe9bc3e77e5a22898e3b9829c7aa0cb90
mhristova <mhristova6@gmail.com>
2013-11-26 10:19:33 +0000

    bug fix

--- static/js/individual.js
+++ static/js/individual.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 
-=======
->>>>>>> af88267964d5a20834d53efcebd8a77b52379a6f
 $(document).ready(function() { 
 	checkLogin();
 	loadPage($.cookie("index"));
@@ -22,13 +19,7 @@ function loadPage(index){
 			document.getElementById('CurrentPriceArrow').className = "fa fa-arrow-up ArrowUp";
 	});
 }
-
-<<<<<<< HEAD
 function follow(){}
-=======
-function follow(){
-
-
 
 function checkLogin() {
 	var login = document.getElementById("logCond");
@@ -44,34 +35,3 @@ function checkLogin() {
 		login.innerHTML = '<a href="/login.html">LOGIN</a>';
 	}
 }
-
-$(document).ready(function() {
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-		checkLogin();
-
-	} else {
-	  alert('The File APIs are not fully supported in this browser.');
-	}
-})
-
-function checkLogin() {
-	var login = document.getElementById("logCond");
-	var cookie = document.cookie;
-	if (cookie.indexOf("stock_auth_token=") !== -1) {
-		var cookieValue = cookie.indexOf(cookie.indexOf("stock_auth_token="));
-		if (cookieValue !== -1) {
-			login.innerHTML = '<a href="javascript:logout();">LOGOUT</a>'
-		} else {
-			login.innerHTML = '<a href="/login.html">LOGIN</a>';
-		}
-	} else {
-		login.innerHTML = '<a href="/login.html">LOGIN</a>';
-	}
-}
-
-function logout() {
-	document.cookie = 'stock_auth_token=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-	window.location.replace("/main_page.html");
-}
-
->>>>>>> af88267964d5a20834d53efcebd8a77b52379a6f
